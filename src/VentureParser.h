3b81c8835b90d7e89ab71736644604a4b83fa59d
Yura Perov <yura.perov@gmail.com>
2012-11-21 06:46:33 -0500

    Added copyright note

--- src/VentureParser.h
+++ src/VentureParser.h
@@ -2,6 +2,8 @@
 #ifndef VENTURE___VENTURE_PARSER_H
 #define VENTURE___VENTURE_PARSER_H
 
+// Parsed implementation is inspired by: http://howtowriteaprogram.blogspot.com/2010/11/lisp-interpreter-in-90-lines-of-c.html
+
 #include "Header.h"
 
 // TBA
