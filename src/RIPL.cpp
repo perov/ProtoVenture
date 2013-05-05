db115c3e9accb98dc07e30fc325b2485a8293ee7
ardavans <ardavans@mit.edu>
2013-05-05 00:08:25 -0400

    Update

--- src/RIPL.cpp
+++ src/RIPL.cpp
@@ -6,6 +6,7 @@
 #include "ExternalXRPInterface.h"
 #endif
 
+#include "Shell/PrimitiveProbabilisticProcedures.h"
 
 // Should be called DirectiveEntry!
 directive_entry::directive_entry(string directice_as_string,
@@ -525,6 +526,21 @@ void BindStandardElementsToGlobalEnvironment() {
   BindToEnvironment(global_environment,
                     shared_ptr<VentureSymbol>(new VentureSymbol("load-python-Shell-module")), // Make just via the std::string?
                     shared_ptr<VentureXRP>(new VentureXRP(shared_ptr<XRP>(new Primitive_LoadPythonShellModule()))));
+  BindToEnvironment(global_environment,
+                    shared_ptr<VentureSymbol>(new VentureSymbol("generate-empty-surface-and-pmap-prior")), // Make just via the std::string?
+                    shared_ptr<VentureXRP>(new VentureXRP(shared_ptr<XRP>(new Primitive__GenerateEmptySurfaceAndPMapPrior()))));
+  BindToEnvironment(global_environment,
+                    shared_ptr<VentureSymbol>(new VentureSymbol("update-pmap-and-add-lobe")), // Make just via the std::string?
+                    shared_ptr<VentureXRP>(new VentureXRP(shared_ptr<XRP>(new Primitive__UpdatePMapAndAddLobe()))));
+  BindToEnvironment(global_environment,
+                    shared_ptr<VentureSymbol>(new VentureSymbol("get-lobe-pos-x")), // Make just via the std::string?
+                    shared_ptr<VentureXRP>(new VentureXRP(shared_ptr<XRP>(new ERP__GetLobePosX()))));
+  BindToEnvironment(global_environment,
+                    shared_ptr<VentureSymbol>(new VentureSymbol("get-lobe-pos-y")), // Make just via the std::string?
+                    shared_ptr<VentureXRP>(new VentureXRP(shared_ptr<XRP>(new ERP__GetLobePosY()))));
+  BindToEnvironment(global_environment,
+                    shared_ptr<VentureSymbol>(new VentureSymbol("noisy-drill-well")), // Make just via the std::string?
+                    shared_ptr<VentureXRP>(new VentureXRP(shared_ptr<XRP>(new ERP__NoisyDrillWell()))));
 }
 
 
