78f06867d9a3ee8c196529d5095d4eb950579e7b
Yura Perov <yura.perov@gmail.com>
2013-06-26 23:20:40 -0400

    Forgot to comment something.

--- src/Main.cpp
+++ src/Main.cpp
@@ -47,7 +47,7 @@ bool need_to_return_inference;
 void InitGSL() {
   random_generator = gsl_rng_alloc(gsl_rng_mt19937);
   unsigned long seed = static_cast<unsigned long>(time(NULL)); // time(NULL)
-  if (true) {
+  if (false) {
     seed = 1372291726;
     cout << "WARNING: RANDOM SEED is not random!" << endl;
   }
