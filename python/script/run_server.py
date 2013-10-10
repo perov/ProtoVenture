16d01b60959d19665a7be3ea87d10bc859bdbc54
Vlad Firoiu <vladfi2@gmail.com>
2013-10-10 13:00:46 -0400

    run_server script takes --lite

--- python/script/run_server.py
+++ python/script/run_server.py
@@ -4,8 +4,16 @@ import sys
 from venture.shortcuts import *
 
 port = 8082
-if len(sys.argv) > 1:
-    port = int(sys.argv[1])
+lite = False
 
-server = make_ripl_rest_server()
+for arg in sys.argv[1:]:
+    if arg == "--lite":
+        lite = True
+    else:
+        try:
+            port = int(arg)
+        except ValueError:
+            print("Unknown argument " + arg)
+
+server = make_ripl_rest_server(lite=lite)
 server.run(host='0.0.0.0', port=port, debug=True, use_reloader=False)
